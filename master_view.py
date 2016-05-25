691bf7fb0d8a8c98bead81664ad28a976bd6df7c
m-sachi <melisa.sachi@student.uts.edu.au>
2016-05-25 17:10:07 +1000

    Update master_view.py
    + Added comments
    + Closed file_out after writing to it
    + Renamed variable to file_in
    + Changed decrypt_valuables to print in plaintext

--- master_view.py
+++ master_view.py
@@ -2,36 +2,45 @@ import os
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
-import base64
 
+# Name of the file where the decryption key is stored
 decryption_key_file = 'decryption_key.txt'
 # TODO: Get a better password
 password = "testtest"
 
 def decrypt_valuables(f):
     decryption_key = get_decryption_key_from_file()
-    decode_cipher = PKCS1_v1_5.new(decryption_key)
-    decoded_text = decode_cipher.decrypt(f, "Error")
-    print("Decoded text: {}".format(decoded_text))
+    cipher = PKCS1_v1_5.new(decryption_key)
+    # Decode the ciphertext, and return 'error' if it fails
+    decoded_text = cipher.decrypt(f, "Error")
+    print(str(decoded_text, "ascii"))
 
 def get_decryption_key_from_file():
-    file = open(decryption_key_file, 'rb')
-    encrypted_key = file.read()
-    file.close()
+    # Open file and read the encrypted key
+    file_in = open(decryption_key_file, 'rb')
+    encrypted_key = file_in.read()
+    file_in.close()
+
+    # Get the key, using the password to decrypt it
     decryption_key = RSA.importKey(encrypted_key, passphrase=password)
     #print("Private key read: {}".format(decryption_key.exportKey()))
     return decryption_key
 
-def generate_key_pair():
+def generate_encryption_keys():
+    # Generate 4096-bit public and private keys
     key = RSA.generate(4096)
 
+    # Export the public key
     public_key = key.publickey()
     public_key = public_key.exportKey()
     print("Public Key: {}".format(public_key))
 
+    # Encrypt and export the private key
     encrypted_private_key = key.exportKey(passphrase=password, pkcs=8)
+    # Write the private key to file
     file_out = open(decryption_key_file, "wb")
     file_out.write(encrypted_private_key)
+    file_out.close
     print("Private key: {}".format(key.exportKey()))
 
 

